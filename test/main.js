445e33bf0bcd70e5b0f6f9697d75fbd3e51c04b7
Gnibeda <anton.donnu@gmail.com>
2015-09-11 16:10:34 +0300

    test

--- test/main.js
+++ test/main.js
@@ -2,10 +2,21 @@ var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
 
+function isChrome() {
+    var isChromium = window.chrome,
+        vendorName = window.navigator.vendor,
+        isOpera = window.navigator.userAgent.indexOf("OPR") > -1;
+    if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function fireEvent(name, x, y, chart) {
     var event = document.createEvent("MouseEvents");
     event.initMouseEvent(
-        name, true, true, chart.screen.el, null, x + 400, y + 40, x + 400, y + 40,
+        name, true, true, window, null, x + 400, y + 40, x + 400, y + 40,
         false, false, false, false,
         0, null
     );
